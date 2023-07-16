66dc76a7139044fe0b1600c707e864f48d20fcd9
mostafa ahmed <mostafa.ahmed2509@gmail.com>
2023-07-16 16:57:46 +0300

    convert to scss

--- src/app/list/item/item.component.ts
+++ src/app/list/item/item.component.ts
@@ -3,7 +3,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.sass']
+  styleUrls: ['./item.component.scss']
 })
 export class ItemComponent {
 
--- src/app/list/list.component.ts
+++ src/app/list/list.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
-  styleUrls: ['./list.component.sass']
+  styleUrls: ['./list.component.scss']
 })
 export class ListComponent {
   selected =1;
